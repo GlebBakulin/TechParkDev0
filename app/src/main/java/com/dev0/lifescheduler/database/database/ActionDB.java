13cfeb28412965f1bce892a6cccbaa773c551205
Danila Nakonechny <chessmate13@mail.ru>
2019-12-23 19:40:15 +0300

    Merge branch 'dev' into dan2
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/java/com/dev0/lifescheduler/AddTaskFragment.java
    #	build.gradle

---
+++ app/src/main/java/com/dev0/lifescheduler/database/database/ActionDB.java
@@ -0,0 +1,18 @@
+package com.dev0.lifescheduler.database.database;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+import com.dev0.lifescheduler.ApplicationScheduler;
+import com.dev0.lifescheduler.database.dao.ActionDao;
+import com.dev0.lifescheduler.database.entity.ActionEntity;
+
+@Database(entities = {ActionEntity.class}, version = 1)
+public abstract class ActionDB extends RoomDatabase {
+    public static ActionDB getDB() {
+        return ApplicationScheduler.getInstance().getDatabase();
+    }
+
+    public abstract ActionDao actionDao();
+
+}
